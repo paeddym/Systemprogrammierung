5764e91b34978e2c408a73e3366c078a49c34899
Stefan Gast <stefan.gast@rwu.de>
2019-12-04 15:57:08 +0100

    network: Add TODO about message structs
    Add a TODO comment telling the students to define the message structures
    for the network protocol when they are implementing the login after the
    first exercise sheet. The layout of the structures can be derived from a
    document which can be downloaded from Moodle.
    
    Thanks to Gerald Lautenschlager <gerald.lautenschlager@rwu.de> for this
    suggestion.

--- src/network.h
+++ src/network.h
@@ -3,8 +3,10 @@
 
 #include <stdint.h>
 
+/* TODO: When implementing the fully-featured network protocol (including
+ * login), replace this with message structures derived from the network
+ * protocol (RFC) as found in the moodle course. */
 enum { MSG_MAX = 1024 };
-
 typedef struct __attribute__((packed))
 {
 	uint16_t len;		//real length of the text (big endian, len <= MSG_MAX)
