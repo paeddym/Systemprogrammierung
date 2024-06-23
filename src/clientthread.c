6b2beb77cfe75da582c1277109016753527a78e6
Can Dogan <cd-224252@hs-weingarten.de>
2024-06-23 12:45:17 +0200

    clienthread has now login

--- src/clientthread.c
+++ src/clientthread.c
@@ -3,32 +3,158 @@
 #include "util.h"
 #include <string.h> 
 
-#define MAX_BUFFER_SIZE  1024
-
 
 void *clientthread(void *arg){
 	User *self = (User *)arg;
 
 	debugPrint("Started client thread");
 
-	int bytes_received;
-	char buf[MAX_BUFFER_SIZE];
-
-	while(1)
+	Message *buffer = calloc(1, sizeof(Message));
+	if (buffer == NULL)
 	{
-		bytes_received = recv(self->sock, buf, MAX_BUFFER_SIZE -1, 0);
-		if (bytes_received == -1) {
-    		perror("Failed to receive socket data!");
-		} else if (bytes_received == 0) {
-			removeUser(self);
-			perror("Closed connection by peer");
-		} else {
-			buf[bytes_received] = '\0';
-			iterateList(sendMessageToClient, self, buf);
-		}			
+    	perror("Memory allocation failed");
+    	return NULL;
 	}
+
+	//Receive LoginRequest
+	Message loginRequest;
+	int status = networkReceive(self->sock, &loginRequest);
+	int nameLength = lengthOfName(&loginRequest);
+	char clientName[nameMax];
+	memcpy(clientName, loginRequest.body.loginRequest.requestName, nameLength);
+	lockUser();
+	if(status != noError){
+		close(self->sock);
+		unlockUser();
+		return NULL;
+	}
+
+	//Sending LoginResponse
+	Message loginResponse;
+	char *sName = "chatbot server";
+	loginResponse = initMessage(loginResponseType);
+	loginResponse.body.loginResponse.code = 0;
+	loginResponse.header.length = sizeof(loginResponse.body.loginResponse.code) + sizeof(loginResponse.body.loginResponse.responseName) + sizeof(loginResponse.body.loginResponse.magic);
+	memcpy(loginResponse.body.loginResponse.responseName, sName, strlen(sName));
+	memcpy(self->name , clientName, strlen(clientName));//name gespeichert im user
+
+	//user added
+
+	while(1){
+		//Receive messages
+		Message clientToServer;
+		int status = networkReceive(self->sock, &clientToServer);
+		if(status != noError){
+			//todo
+		}
+
+		char *command = extractCommand(clientToServer.body.clientToServer.text);
+
+		Message serverToClient;
+		serverToClient = initMessage(server2ClientType); //type and timestamp set
+		strncpy(serverToClient.body.serverToClient.sender, '\0', sizeof(char));
+
+		if(clientToServer.body.clientToServer.text[0] == '/'){//Befehl
+			
+			int admin = 1;
+			if(admin == 1/*admin*/){
+				if(command == 'kick'){
+					//todo user kicken
+					//original sender setzen?
+				}
+				else if(command == 'pause'){
+					//serverToClient message
+					strncpy(serverToClient.body.serverToClient.text, "the chat has been paused", sizeof(serverToClient.body.serverToClient.text));
+				}
+				else if(command == 'resume'){
+					//serverToClient message
+					strncpy(serverToClient.body.serverToClient.text, "the chat has been resumed", sizeof(serverToClient.body.serverToClient.text));
+				}
+				/*The chat has been paused or resumed successfully. In these cases,
+  				the server shall send a Server2Client message containing an
+  				implementation-defined Text to every client that is currently logged
+  				in.*/
+				else{
+					//command invalid
+					//serverToClient message
+					//OriginalSender starting with null and the Text 
+					//containing an implementation-defined error message
+					/*A client has sent an invalid command to the server. In that case,
+	  				the server shall send a Server2Client message with an
+	  				implementation-defined Text to the client which has sent the
+	  				command.*/
+
+					
+				}
+			}
+			else{
+				//fehler
+				/*A client has sent an invalid command to the server. In that case,
+	  			the server shall send a Server2Client message with an
+	  			implementation-defined Text to the client which has sent the
+	  			command.*/
+				//Sagen dass admin rechte nicht vorhanden waren
+
+			}
+		}
+		else if(clientToServer.body.clientToServer.text[0] != '/'){//Normale Nachricht
+
+			//serverToClient message
+
+			//Send them to all users, skip self
+			
+			//memset(serverToClient.body.serverToClient.originalSender, '\0', 32); // Mit Nullen initialisieren
+			memcpy(serverToClient.body.serverToClient.sender, self->name, strlen(self->name));
+			memcpy(serverToClient.body.serverToClient.text, clientToServer.body.clientToServer.text, strlen(clientToServer.body.clientToServer.text));
+			iterateUsers(networkSend, self, &serverToClient);
+		}
+		else{
+			/*Server2Client message with an implementation-defined Text to the
+	  		client which has sent the command. The same applies if a command fails for another reason, or if there
+	  		is a general problem with forwarding regular text messages to the
+	  		client (like: server message queue is full*/
+		}
+		serverToClient.header.length = sizeof(serverToClient.body.serverToClient.timestamp) + sizeof(serverToClient.body.serverToClient.sender) + sizeof(serverToClient.body.serverToClient.text);
+	}
+
+	if(clientName[0] == '\0'){
+		close(self->sock);
+		return NULL;
+	}
+
+	free(buffer);
 	debugPrint("Client thread stopping.");
 	return NULL;
+}
+
+int lengthOfName(Message *buffer){
+	int length;
+	if(buffer->header.type == loginRequestType){
+		length = buffer->header.length - sizeof(buffer->body.loginRequest.magic) - sizeof(buffer->body.loginRequest.version);
+		return length;
+	}
+	if(buffer->header.type == client2ServerType){
+		length = buffer->header.length;
+		return length;
+	}
+	return error;
+}
+
+const char* extractCommand(const char *text) {
+    const char *start = strchr(text, '/');
+    if (start != NULL) {
+        start++; // Gehe zum Zeichen nach dem Slash
+        const char *end = strchr(start, ' '); 
+        if (end == NULL) {
+            end = strchr(start, '\0'); // Wenn kein Leerzeichen gefunden wird, gehe bis zum String-Ende
+        }
+        static char command[20];
+        strncpy(command, start, end - start);
+        command[end - start] = '\0';
+        return command;
+    } else {
+        //todo
+    }
 }
 
 void sendMessageToClient(User *currentUser, char *buf) {
