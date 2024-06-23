b4c55d4fa4e00eff2dcd42294407dc8161265d83
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-06-23 10:38:26 +0200

    Added basic send/receive functionality and primitive error detection

--- src/network.c
+++ src/network.c
@@ -3,21 +3,73 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <stdbool.h>
 
 int networkReceive(int fd, Message *buffer)
 {
 	ssize_t receivedBytes = recv(fd, &buffer->header, sizeof(buffer->header), MSG_WAITALL);
-	//TODO: Convert length byte order
-	//TODO: Validate length
-	//TODO: Receive text
+
+	printf("Receiving message with length %d and type %d\n", buffer->header.length, buffer->header.type);
+
+	if (receivedBytes == -1) {
+		perror("Failed to receive message!");
+		return -1;
+	} else if (receivedBytes == 0) {
+		perror("Connection has been closed by the client!");
+		return -1;
+	}
+
+	buffer->header.length = ntohs(buffer->header.length);	// Convert to host byte order
+	if(isHeaderValid(buffer->header.type, buffer->header.length) == false){
+		perror("Message with invalid header received!");
+		return -1;
+	}
+
+
+	receivedBytes = recv(fd, &buffer->body, buffer->header.length, MSG_WAITALL);
+	printf("Received %ld bytes\n", receivedBytes);
+	if(receivedBytes == 0){
+		perror("Connection has been closed by the client!");
+		return -1;
+	} else if(receivedBytes == -1){
+		perror("Failed to receive message!");
+		return -1;
+	}
+	printf("Message content: %s\n", buffer->body.clientToServer.text);
+
+	if(isMessageValid(buffer) == false){
+		perror("Message with invalid body received!");
+		return -1;
+	}
 
 	return -1;
 }
 
 int networkSend(int fd, const Message *buffer)
 {
-	//TODO: Send complete message
+	printf("Sending message:\nlength = %d\ntype = %d\ncontent = %s\n", buffer->header.length, buffer->header.type, buffer->body.serverToClient.text);
 
-	errno = ENOSYS;
+	ssize_t sentBytes = send(fd, &buffer->header, sizeof(buffer->header), 0);
+	if (sentBytes == -1) {
+		perror("Failed to send message header!");
+		return -1;
+	}
+
+	sentBytes = send(fd, &buffer->body, buffer->header.length, 0);
+	if (sentBytes == -1) {
+		perror("Failed to send message body!");
+		return -1;
+	}
+
 	return -1;
+}
+
+bool isHeaderValid(uint8_t type, uint16_t length){
+	//Implement validation
+	return false;
+}
+
+bool isMessageValid(Message *buffer){
+	//Implement validation
+	return false;
 }
